cc7394e44a9b7aafc16e8bedd8b0927048f39c88
Myrian Ayala <mirianayala11@gmail.com>
2019-02-07 11:48:13 -0300

    Este es el archivo dino.py modificado

---
+++ Dino.py
@@ -0,0 +1,24 @@
+
+class Dino:
+    #cola,dientes,color,size
+  def __init__(self):#siempre para empezar a construir
+      print("Hola soy un dinosaurio pero no se quien soy")
+
+#Ejercicio: Agregarle una propiedad color a la clase Dino y que salude
+#diciendo de que color es el dinosaurio
+
+class Dino1:
+  def __init__(self, nombre, color):#siempre para empezar a construir
+      print("Hola soy un dinosaurio, me llamo",nombre, "y soy de color",color)
+
+
+pepito=Dino1("Pepe","Rojo") #pepito es un objeto con esas caracteristicas y
+                            #esos valores se almacenan en la funciòn 
+                            # "init" permite que el objeto nazca
+                            # "self" hace que se refiera a si mismo porque 
+                            #esta en el mismo archivo
+
+#tercer
+#crear propiedades/atributos
+
+
